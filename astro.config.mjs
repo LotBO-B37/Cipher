ae714055626cba9460c186d09b5e2c2e6a1d124a
LotBO-B37 <robdawson13072477@gmail.com>
2026-06-19 14:51:36 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://your-site-name.netlify.app'
+  output: "static",
 });
